287a43d14f2484f93f2f6777934405ef199f757e
James Yu <jamesyu@jamess-mbp-2.attlocal.net>
2016-10-14 11:58:57 -0500

    Initial commit

---
+++ WifiLocation.cpp
@@ -0,0 +1,163 @@
+#include "WifiLocation.h"
+
+static const int E_OK = 0;
+static const int E_NOCONNECTION = -1;
+
+WifiLocation::WifiLocation(Client* client,
+                          const char* token,
+                          const char* host,
+                          int port)
+{
+  _client = client;
+  _token = token;
+  _host = host;
+  _port = port;
+  _location.address = "N/A";
+  _location.longitude = 0.0;
+  _location.latitude = 0.0;
+  _location.accuracy = 0;
+}
+
+int WifiLocation::connect(void)
+{
+  if(!_client->connect(_host, _port))
+  {
+    return E_NOCONNECTION;
+  }
+
+  return E_OK;
+}
+
+Location WifiLocation::lastKnownLocation(void)
+{
+  return _location;
+}
+
+Location WifiLocation::updateLocation(WifiAP wifiAPs[], int count)
+{
+  Location newLocation;
+  newLocation.address = "N/A";
+  newLocation.longitude = 0.0;
+  newLocation.latitude = 0.0;
+  newLocation.accuracy = 0;
+
+  if(!_client->connected())
+  {
+    //return newLocation;
+  }
+
+  if(!connect())
+  {
+    //return newLocation;
+  }
+
+  if(count > 15)
+  {
+    quickSort(wifiAPs, 0, count-1);
+  }
+
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["token"] = _token;
+  root["address"] = 1;
+
+  JsonArray& wifis = root.createNestedArray("wifi");
+  int numWifi = (count > 15) ? 15 : count;
+  for(int i=0; i<numWifi; i++)
+  {
+    JsonObject& wifi = wifis.createNestedObject();
+    wifi["bssid"] = wifiAPs[i].bssid;
+    wifi["rssi"] = wifiAPs[i].rssi;
+    wifi["channel"] = wifiAPs[i].channel;
+  }
+
+  char buff[root.measureLength() + 1];
+  root.printTo(buff, sizeof(buff));
+  String strBuff(buff);
+  strBuff += "\n";
+
+  String request = "POST " + (String)ENDPOINT + " HTTP/1.1\r\n" +
+    "Host: " + _host + "\r\n" +
+    "Content-Type: application/json\r\n" +
+    "Content-Length: " + strBuff.length() + "\r\n\r\n" + strBuff;
+
+  String response = "";
+  String chunk = "";
+  int limit = 0;
+  int sendCount = 0;
+
+  _client->print(request);
+  do
+  {
+    if (_client->connected())
+    {
+      chunk = _client->readStringUntil('\n');
+      response += chunk;
+    }
+    limit++;
+  } while (chunk.length() > 0 && limit < 100);
+  close();
+
+  if (response.length() > 12)
+  {
+    String httpCode = getHttpCode(response);
+    if(httpCode == "200")
+    {
+      String respBody = response.substring(response.indexOf('{'), response.indexOf('}') + 1);
+      JsonObject& root2 = jsonBuffer.parseObject(respBody);
+
+      if (!root.success())
+      {
+        return newLocation;
+      }
+
+      newLocation.address = root2["address"].asString();
+      newLocation.longitude = root2["lon"];
+      newLocation.latitude = root2["lat"];
+      newLocation.accuracy = root2["accuracy"];
+      _location = newLocation;
+
+      return _location;
+    }
+  }
+}
+
+String WifiLocation::getHttpCode(String resp)
+{
+  int periodIndex = resp.indexOf('.');
+  return resp.substring(periodIndex + 3, periodIndex + 6);
+}
+
+void WifiLocation::quickSort(WifiAP arr[], int left, int right)
+{
+  int i = left, j = right;
+  WifiAP tmp;
+  int pivot = arr[((left + right) / 2)].rssi;
+
+  /* partition */
+  while (i <= j) {
+        while (arr[i].rssi > pivot)
+              i++;
+        while (arr[j].rssi < pivot)
+              j--;
+        if (i <= j) {
+              tmp = arr[i];
+              arr[i] = arr[j];
+              arr[j] = tmp;
+              i++;
+              j--;
+        }
+  };
+
+  /* recursion */
+  if (left < j)
+        quickSort(arr, left, j);
+  if (i < right)
+        quickSort(arr, i, right);
+}
+
+void WifiLocation::close(void)
+{
+  _client->flush();
+  _client->stop();
+}
---
+++ WifiLocation.h
@@ -0,0 +1,51 @@
+#ifndef WIFILOCATION_H
+#define WIFILOCATION_H
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include "Client.h"
+
+static const char* DEFAULT_HOST = "us2.unwiredlabs.com";
+static const int DEFAULT_PORT = 443;
+static const char* ENDPOINT = "/v2/process.php";
+
+typedef struct Location
+{
+  String address;
+  double longitude;
+  double latitude;
+  int accuracy;
+} Location;
+
+typedef struct WifiAP
+{
+  String bssid;
+  int rssi;
+  int channel;
+} WifiAP;
+
+class WifiLocation
+{
+  public:
+    WifiLocation(Client* client,
+                const char* token,
+                const char* host = DEFAULT_HOST,
+                int port = DEFAULT_PORT);
+
+    Location updateLocation(WifiAP wifis[], int count);
+    Location lastKnownLocation(void);
+
+  private:
+    Client* _client;
+    const char* _token;
+    const char* _host;
+    int _port;
+    Location _location;
+
+    int connect(void);
+    String getHttpCode(String resp);
+    void close(void);
+    void quickSort(WifiAP arr[], int left, int right);
+};
+
+#endif
